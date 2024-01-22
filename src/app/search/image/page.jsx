c822cf3728cb4191d1d5caa8aa780bbce9fb5c43
YASH GUPTA <yash2002.gupta@gmail.com>
2024-01-22 12:01:50 +0530

    fetch delay removed

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 export default async function ImageSearchPage({searchParams}) {
   const startIndex = +searchParams.start || "1"
-  await new Promise(r => setTimeout(r, 2000));
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`)
 
   if (!response.ok) {
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 
 export default async function WebSearchPage({searchParams}) {
   const startIndex = +searchParams.start || "1"
-  await new Promise(r => setTimeout(r, 2000));
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`)
 
   if (!response.ok) {
